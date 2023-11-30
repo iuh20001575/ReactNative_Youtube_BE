f13c7f4f823ead92a9a6f46b402a680466c1ead3
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-11-30 17:46:31 +0700

    Update POST

--- api/server.js
+++ api/server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const fs = require('fs');
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const db = JSON.parse(fs.readFileSync(db.json));
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT ?? 8888;
